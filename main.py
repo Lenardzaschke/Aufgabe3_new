a4d7319943a1967880618f7ae2fcb802d0457142
Lenardzaschke <zl4041@mci4me.at>
2024-04-08 12:17:48 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from calculation import calc_mean
+from my_functions import estimate_max_hr, build_person, build_experiment
+
+if __name__ == "__main__":
+    liste = [1,2,3]
+
+print(calc_mean(liste))
+
+#tes  = build_person("Hans", 25, 1.80, 80)
+
+#print(estimate_max_hr)
